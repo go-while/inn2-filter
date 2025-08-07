2656edff3389f2764348f2bf0e0eeb1dd385d487
go-while <go-while@users.noreply.github.com>
2025-08-07 08:22:56 +0100

    Update rsync.sh

--- rsync.sh
+++ rsync.sh
@@ -1,2 +1,4 @@
-rsync -va --progress etc/news/filter/filter_nnrpd.pl root@reader-nyc.newsdeef.eu:/etc/news/filter/filter_nnrpd.pl.new
+rsync -va --progress etc/news/filter/filter_nnrpd.pl root@reader-nyc.newsdeef.eu:/etc/news/filter/filter_nnrpd.pl
 rsync -va --progress news/ root@reader-nyc.newsdeef.eu:/news/
+ssh root@reader-nyc.newsdeef.eu "chown news:news -R /news /etc/news/filter; chmod 700 /news; ctlinnd reload all filter"
+
